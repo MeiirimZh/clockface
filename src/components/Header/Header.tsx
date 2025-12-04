f7cedaca84a449344ee03a3c4869d9d5c960b72c
MeiirimZh <zh.meyirim@gmail.com>
2025-12-04 09:01:51 +0500

    added Header component and wrapper class

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
+
 import './Header.css'
 
 export default function Header() {
     return (
         <header className="header">
             <div className="wrapper">
+                <h1><Link to="/">ClockFace</Link></h1>
+
+                <nav>
+                    <ul>
+                        <li><Link to="/">Settings</Link></li>
+                    </ul>
+                </nav>
             </div>
         </header>
     )
